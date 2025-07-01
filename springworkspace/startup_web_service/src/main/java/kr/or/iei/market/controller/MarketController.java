2a91d77543bbe137cb0b8d5e8eeeaa9074977377
DESKTOP-SJC5QT1\user1 <25chestnut@gmail.com>
2025-07-01 12:14:15 +0900

    marketList 토큰 오류 발생중

--- springworkspace/startup_web_service/src/main/java/kr/or/iei/market/controller/MarketController.java
+++ springworkspace/startup_web_service/src/main/java/kr/or/iei/market/controller/MarketController.java
@@ -1,17 +1,19 @@
 package kr.or.iei.market.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.or.iei.common.annotation.NoTokenCheck;
 import kr.or.iei.common.dto.ResponseDTO;
-import kr.or.iei.member.model.dto.Market;
 import kr.or.iei.market.model.service.MarketService;
 
 @RestController
@@ -22,13 +24,16 @@ public class MarketController {
 	@Autowired
 	private MarketService service;
 	
+	
 	@GetMapping("/list")
-	public ResponseEntity<ResponseDTO> selectMarketList(){
+	@NoTokenCheck
+	public ResponseEntity<ResponseDTO> selectMarketList(@PathVariable int reqPage){
 		ResponseDTO res= new ResponseDTO(HttpStatus.INTERNAL_SERVER_ERROR,"마켓글 조회 중 오류가 발생하였습니다",null,"error");
 		
 		try {
-			ArrayList<Market> marketList=service.selectMarketList();
+			HashMap<String, Object> marketList=service.selectMarketList(reqPage);
 			res = new ResponseDTO(HttpStatus.OK,"", marketList, "");
+			
 		}catch(Exception e) {
 			e.printStackTrace();
 		}
